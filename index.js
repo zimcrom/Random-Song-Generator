6a2a97fefa7292e17f7fe1bfe9f14e8fde208316
Megan H <megannh6349@gmail.com>
2020-09-08 23:01:54 -0400

    added console log statements to count items in array

---
+++ index.js
@@ -0,0 +1,18 @@
+let selectionsEl = document.getElementById("selections");
+let startBtn = document.getElementById("start");
+
+// Get info for random numbers
+console.log("There are " + song.length + " songs");
+console.log("There are " + style.length + " styles");
+
+// // Generate random numbers
+// let randomSongNumber = 
+// let randomStyleNumber = 
+
+// function generateNew() {
+// let getSong = song[randomSongNumber];
+// let getStyle = style[randomStyleNumber];
+// }
+
+// // Generate new selection
+// startBtn.onclick = generateNew;
